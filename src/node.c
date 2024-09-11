dabede35a6d68bdefe5f59822fbe0a2ccc4bc8cc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-11 15:25:19 -0400

    node(feat): Add stubs for initial outline

---
+++ src/node.c
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file src/node.c
+ */
+
+#include "node.h"
+
+#include <stddef.h>
+
+// Function to create a new node.
+Node* node_create(void* data) {
+    return NULL;
+}
+
+// Function to free a node.
+void node_free(Node* node) {
+    return;
+}
