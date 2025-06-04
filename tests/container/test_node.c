8bdb347ed71428d1044d221f0f56d5be3667c4ae
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-04 03:04:27 -0400

    test_node(refactor): Attempt to add a light, transparent, abstraction for setup and teardown

--- tests/container/test_node.c
+++ tests/container/test_node.c
@@ -19,7 +19,7 @@ int test_container_node_create(TestCase* test) {
     ASSERT(
         result == unit->expected,
         "[TestContainerNode] node=%p, object=%p, expected=%d, got=%d",
-        (void*)unit->node,
+        (void*) unit->node,
         unit->node->object,
         unit->expected,
         result
@@ -29,43 +29,37 @@ int test_container_node_create(TestCase* test) {
 }
 
 int test_container_node_create_suite(void) {
-    int a = 5;
-    int b = 3;
-    int c = 7;
+    #define TEST_CASE_CREATE(object_ptr, expected_val) \
+        (TestContainerNode) { \
+            .node = container_node_create((void*) (object_ptr)), \
+            .expected = (expected_val) \
+        }
 
-    ContainerNode* na = container_node_create(&a);
-    ContainerNode* nb = container_node_create(&b);
-    ContainerNode* nc = container_node_create(&c);
-
-    na->next = nb;
-    nb->prev = na;
-    nb->next = nc;
-    nc->prev = nb;
-
+    int a = 5, b = 3, c = 7;
     TestContainerNode cases[] = {
-        { .node = na, .expected = a },
-        { .node = nb, .expected = b },
-        { .node = nc, .expected = c },
+        TEST_CASE_CREATE(&a, a),
+        TEST_CASE_CREATE(&b, b),
+        TEST_CASE_CREATE(&c, c),
     };
 
     size_t total_tests = sizeof(cases) / sizeof(TestContainerNode);
-
     TestCase test_cases[total_tests];
-    for (size_t i = 0; i < total_tests; i++) {
-        test_cases[i].unit = &cases[i];
+    for (size_t i = 0; i < (total_tests); i++) { \
+        (test_cases)[i].unit = &(cases)[i]; \
     }
 
-    TestContext context = {
+    TestContext context ={
         .total_tests = total_tests,
-        .test_name = "Test Container Node Create",
+        .test_name = "Test Node",
         .test_cases = test_cases,
     };
 
     int result = test_unit_run(&context, test_container_node_create, NULL);
 
-    container_node_free(nc);
-    container_node_free(nb);
-    container_node_free(na);
+    for (size_t i = 0; i < total_tests; i++) {
+        container_node_free(cases[i].node);
+    }
+    #undef TEST_CASE_CREATE
 
     return result;
 }
