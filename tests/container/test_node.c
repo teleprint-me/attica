780fe8f89fcc23677697db0bd0d62aa781bc030c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-04 04:22:07 -0400

    test_node(feat): Integrate setup and teardown logic to simplify test suite

--- tests/container/test_node.c
+++ tests/container/test_node.c
@@ -7,66 +7,61 @@
 #include "container/node.h"
 
 typedef struct TestContainerNode {
-    ContainerNode* node;
+    int value;
     int expected;
+    ContainerNode* node;
 } TestContainerNode;
 
-int test_container_node_create(TestCase* test) {
+void test_node_setup(TestCase* test) {
     TestContainerNode* unit = (TestContainerNode*) test->unit;
+    unit->node = container_node_create(&unit->value);
+}
 
+void test_node_teardown(TestCase* test) {
+    TestContainerNode* unit = (TestContainerNode*) test->unit;
+    container_node_free(unit->node);
+}
+
+int test_container_node(TestCase* test) {
+    TestContainerNode* unit = (TestContainerNode*) test->unit;
     int result = *(int*) unit->node->object;
 
     ASSERT(
-        result == unit->expected,
-        "[TestContainerNode] node=%p, object=%p, expected=%d, got=%d",
-        (void*) unit->node,
-        unit->node->object,
-        unit->expected,
-        result
+        result == unit->expected, "[TestContainerNode] expected %d, got %d", unit->expected, result
     );
 
     return 0;
 }
 
-int test_container_node_create_suite(void) {
-    #define TEST_CASE_CREATE(object_ptr, expected_val) \
-        (TestContainerNode) { \
-            .node = container_node_create((void*) (object_ptr)), \
-            .expected = (expected_val) \
-        }
-
-    int a = 5, b = 3, c = 7;
+int test_container_node_suite(void) {
     TestContainerNode cases[] = {
-        TEST_CASE_CREATE(&a, a),
-        TEST_CASE_CREATE(&b, b),
-        TEST_CASE_CREATE(&c, c),
+        {.value = 5, .expected = 5},
+        {.value = 3, .expected = 3},
+        {.value = 7, .expected = 7},
     };
 
     size_t total_tests = sizeof(cases) / sizeof(TestContainerNode);
     TestCase test_cases[total_tests];
-    for (size_t i = 0; i < (total_tests); i++) { \
-        (test_cases)[i].unit = &(cases)[i]; \
+    for (size_t i = 0; i < (total_tests); i++) {
+        (test_cases)[i].unit = &(cases)[i];
     }
 
-    TestContext context ={
+    TestContext context = {
         .total_tests = total_tests,
         .test_name = "Test Node",
         .test_cases = test_cases,
+        .setup_each = test_node_setup,
+        .teardown_each = test_node_teardown,
     };
 
-    int result = test_unit_run(&context, test_container_node_create, NULL);
-
-    for (size_t i = 0; i < total_tests; i++) {
-        container_node_free(cases[i].node);
-    }
-    #undef TEST_CASE_CREATE
+    int result = test_unit_run(&context, test_container_node, NULL);
 
     return result;
 }
 
 int main(void) {
     static TestRegister suites[] = {
-        {"Container Node Create", test_container_node_create_suite},
+        {"Container Node", test_container_node_suite},
     };
 
     int result = 0;
