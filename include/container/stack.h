b42e677b86b58cb05af3ccd07c9e7067fe42ac6d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-04 16:19:18 -0400

    stack(chore): Add C extern and include bool

--- include/container/stack.h
+++ include/container/stack.h
@@ -7,10 +7,15 @@
 #ifndef CONTAINER_STACK_H
 #define CONTAINER_STACK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #include "container/node.h"
 
-#include <stddef.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
 
 typedef struct ContainerStack {
     uint32_t size;
@@ -23,5 +28,9 @@ void container_stack_free(ContainerStack* stack);
 bool  container_stack_push(ContainerStack* stack, void* object);
 void* container_stack_pop(ContainerStack* stack);
 void* container_stack_peek(const ContainerStack* stack);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // CONTAINER_STACK_H
