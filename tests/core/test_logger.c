642557d2356c89456df7a9c397c2ae718c97e5b1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-05 00:42:39 -0400

    test_logger(feat): Integrate updated unit test framework

--- tests/core/test_logger.c
+++ tests/core/test_logger.c
@@ -4,18 +4,23 @@
  * @file tests/core/test_logger.c
  * @brief Automated unit tests for the thread-safe logger implementation.
  *
- * Build:
- *   gcc -o test_logger source/logger.c tests/test_logger.c -lpthread
+ * Manual Build:
+ *   gcc -o test_logger src/core/logger.c src/test/unit.c tests/core/test_logger.c -I./include -lpthread
  *
  * Manual Run:
- *   ./build/tests/test_logger
+ *   ./test_logger
  *   # Displays detailed pass/fail status for each logger test suite.
  *
- * Automated Run (CTest):
- *   ctest --output-on-failure --test-dir build/
+ * CMake Build:
+ *   cmake -B build -DCMAKE_BUILD_TYPE=Debug
+ *   cmake --build build --config Debug -j 16
+ *
+ * CMake Run (CTest):
+ *   ctest --output-on-failure --test-dir build
  *   # Integrates with CMake/CTest; works in CI and locally.
  *
  * Output:
+ *   ./build/tests/core/test_logger
  *   [INFO] ... [RUN] <suite>
  *   [INFO] ... [RESULT] <suite>: <N/N> tests passed
  *   [PASS]/[FAIL] suite status
@@ -83,61 +88,60 @@ static bool file_match(const char* path, const char* message) {
  * @section Test Log Levels
  */
 
-typedef struct LoggerTestLevel {
-    const LogLevel level;
+typedef struct TestLogLevel {
+    const LogLevel log_level;
     const char* message;
-    const bool expected;
-} LoggerTestLevel;
+    const bool expected; // was the message logged?
+} TestLogLevel;
 
-int test_logger_level(TestCase* test) {
-    LoggerTestLevel* unit = (LoggerTestLevel*) test->unit;
+int test_group_log_level(TestUnit* unit) {
+    TestLogLevel* data = (TestLogLevel*) unit->data;
 
     int state = file_capture(stderr, file_temp);
 
     Logger* logger = logger_create(LOG_LEVEL_WARN, LOG_TYPE_STREAM, NULL);
     if (!logger) {
-        LOG_ERROR("[LoggerTestLevel] Failed to create a logger instance!");
+        LOG_ERROR("[TestLogLevel] Failed to create a logger instance!");
         return 1;
     }
-    const bool result = logger_message(logger, unit->level, "%s\n", unit->message);
+    const bool result = logger_message(logger, data->log_level, "%s\n", data->message);
     logger_free(logger);
 
     file_restore(stderr, file_temp, state);
 
     ASSERT(
-        result == unit->expected,
-        "[LoggerTestLevel] level=%d, message=%d expected='%s' got='%s'",
-        unit->level,
-        unit->message,
-        unit->expected ? "true" : "false",
+        result == data->expected,
+        "[TestLogLevel] log_level=%d, message=%d, expected='%s', got='%s'",
+        data->log_level,
+        data->message,
+        data->expected ? "true" : "false",
         result ? "true" : "false"
     );
 
     return 0;
 }
 
-int test_logger_level_suite(void) {
-    static LoggerTestLevel cases[] = {
+int test_suite_log_level(void) {
+    TestLogLevel data[] = {
         {LOG_LEVEL_INFO, "This message should not appear", false},
         {LOG_LEVEL_WARN, "Global logger warning", true},
         {LOG_LEVEL_ERROR, "Global logger error", true},
     };
 
-    size_t total_tests = sizeof(cases) / sizeof(LoggerTestLevel);
-
-    TestCase test_cases[total_tests];
-    for (size_t i = 0; i < total_tests; i++) {
-        test_cases[i].unit = &cases[i];
+    size_t count = sizeof(data) / sizeof(TestLogLevel);
+    TestUnit units[count];
+    for (size_t i = 0; i < count; i++) {
+        units[i].data = &data[i];
     }
 
-    TestContext context = {
-        .total_tests = total_tests,
-        .test_name = "Logger Test Level",
-        .test_cases = test_cases,
+    TestGroup group = {
+        .name = "Test Log Level Group",
+        .count = count,
+        .units = units,
+        .run = test_group_log_level,
     };
 
-    // Return test suite results
-    return test_unit_run(&context, test_logger_level, NULL);
+    return test_group_run(&group);
     ;
 }
 
@@ -145,105 +149,105 @@ int test_logger_level_suite(void) {
  * @section Test Log Files
  */
 
-typedef struct LoggerTestFile {
-    LogLevel logger_level;
+typedef struct TestLogFile {
+    LogLevel log_level;
     LogLevel message_level;
     const char* message;
     bool should_log; // Should this message appear in the file?
-} LoggerTestFile;
+} TestLogFile;
 
-int test_logger_file(TestCase* test) {
-    LoggerTestFile* unit = (LoggerTestFile*) test->unit;
+int test_group_log_file(TestUnit* unit) {
+    TestLogFile* data = (TestLogFile*) unit->data;
 
     int state = file_capture(stderr, file_temp);
 
-    Logger* logger = logger_create(unit->logger_level, LOG_TYPE_FILE, file_temp);
+    Logger* logger = logger_create(data->log_level, LOG_TYPE_FILE, file_temp);
     if (!logger) {
-        LOG_ERROR("[LoggerTestFile] Failed to create a logger instance!");
+        LOG_ERROR("[TestLogFile] Failed to create a logger instance!");
         return 1;
     }
-    logger_message(logger, unit->message_level, "%s", unit->message);
+    logger_message(logger, data->message_level, "%s", data->message);
     logger_free(logger);
 
-    const bool match = file_match(file_temp, unit->message);
+    const bool match = file_match(file_temp, data->message);
     file_restore(stderr, file_temp, state);
 
     ASSERT(
-        match == unit->should_log,
-        "[LoggerTestFile] logger_level=%d, message_level=%d, expected='%s', got='%s'",
-        unit->logger_level,
-        unit->message_level,
-        unit->should_log ? "present" : "absent",
+        match == data->should_log,
+        "[TestLogFile] log_level=%d, message_level=%d, expected='%s', got='%s'",
+        data->log_level,
+        data->message_level,
+        data->should_log ? "present" : "absent",
         match ? "present" : "absent"
     );
 
     return 0;
 }
 
-int test_logger_file_suite(void) {
-    static LoggerTestFile cases[] = {
+int test_suite_log_file(void) {
+    TestLogFile data[] = {
         {LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, "Debug should not log", false},
         {LOG_LEVEL_INFO, LOG_LEVEL_INFO, "Info should log", true},
         {LOG_LEVEL_INFO, LOG_LEVEL_ERROR, "Error should log", true},
     };
 
-    size_t total_tests = sizeof(cases) / sizeof(LoggerTestFile);
-
-    TestCase test_cases[total_tests];
-    for (size_t i = 0; i < total_tests; i++) {
-        test_cases[i].unit = &cases[i];
+    size_t count = sizeof(data) / sizeof(TestLogFile);
+    TestUnit units[count];
+    for (size_t i = 0; i < count; i++) {
+        units[i].data = &data[i];
     }
 
-    TestContext context = {
-        .total_tests = total_tests,
-        .test_name = "Logger Test File",
-        .test_cases = test_cases,
+    TestGroup group = {
+        .name = "Test Log File Group",
+        .count = count,
+        .units = units,
+        .run = test_group_log_file,
     };
 
-    return test_unit_run(&context, test_logger_file, NULL);
+    return test_group_run(&group);
 }
 
 /**
  * @section Test Global Logger
  */
 
-typedef struct LoggerTestGlobal {
-    LogLevel logger_level; // What level is the global logger set to?
+typedef struct TestGlobalLogger {
+    LogLevel log_level; // What level is the global logger set to?
     LogLevel message_level; // What level is the log message?
     const char* message;
-    bool should_log; // Should the message be logged, given the logger_level?
-} LoggerTestGlobal;
+    bool should_log; // Should the message be logged, given the log_level?
+} TestGlobalLogger;
 
-int test_logger_global(TestCase* test) {
-    LoggerTestGlobal* unit = (LoggerTestGlobal*) test->unit;
+int test_group_global_logger(TestUnit* unit) {
+    TestGlobalLogger* data = (TestGlobalLogger*) unit->data;
 
     int state = file_capture(stderr, file_temp);
 
     // Set up global logger to desired level
-    logger_set_global(unit->logger_level, LOG_TYPE_STREAM, "stream", stderr, NULL);
+    logger_set_global(data->log_level, LOG_TYPE_STREAM, "stream", stderr, NULL);
 
     // Use LOG_* macro (or direct logger_message for even tighter focus)
-    LOG(&logger_global, unit->message_level, "%s", unit->message);
+    LOG(&logger_global, data->message_level, "%s", data->message);
 
-    const bool match = file_match(file_temp, unit->message);
+    const bool match = file_match(file_temp, data->message);
     file_restore(stderr, file_temp, state);
 
     logger_set_global(LOG_LEVEL_DEBUG, LOG_TYPE_STREAM, "stream", NULL, NULL); // Reset
 
     ASSERT(
-        match == unit->should_log,
-        "[LoggerTestGlobal] logger_level=%d, message_level=%d, expected='%s', got='%s'",
-        unit->logger_level,
-        unit->message_level,
-        unit->should_log ? "present" : "absent",
+        match == data->should_log,
+        "[TestGlobalLogger] log_level=%d, message_level=%d, expected='%s', got='%s'",
+        data->log_level,
+        data->message_level,
+        data->should_log ? "present" : "absent",
         match ? "present" : "absent"
     );
 
     return 0;
 }
 
-int test_logger_global_suite(void) {
-    static LoggerTestGlobal cases[] = {
+int test_suite_global_logger(void) {
+    static TestGlobalLogger data[] = {
         {LOG_LEVEL_WARN, LOG_LEVEL_INFO, "This message should not appear", false},
         {LOG_LEVEL_WARN, LOG_LEVEL_WARN, "Global logger warning", true},
         {LOG_LEVEL_WARN, LOG_LEVEL_ERROR, "Global logger error", true},
@@ -251,56 +255,56 @@ int test_logger_global_suite(void) {
         {LOG_LEVEL_DEBUG, LOG_LEVEL_INFO, "Debug logger: info logs", true},
     };
 
-    size_t total_tests = sizeof(cases) / sizeof(LoggerTestGlobal);
-
-    TestCase test_cases[total_tests];
-    for (size_t i = 0; i < total_tests; i++) {
-        test_cases[i].unit = &cases[i];
+    size_t count = sizeof(data) / sizeof(TestGlobalLogger);
+    TestUnit units[count];
+    for (size_t i = 0; i < count; i++) {
+        units[i].data = &data[i];
     }
 
-    TestContext context = {
-        .total_tests = total_tests,
-        .test_name = "Logger Global",
-        .test_cases = test_cases,
+    TestGroup group = {
+        .name = "Test Global Logger Group",
+        .count = count,
+        .units = units,
+        .run = test_group_global_logger,
     };
 
-    return test_unit_run(&context, test_logger_global, NULL);
+    return test_group_run(&group);
 }
 
 /**
  * @section Test Lazy Initialization
  */
 
-typedef struct LoggerTestLazy {
-    LogLevel log_level; // Level to create the logger with
+typedef struct TestLazyLogger {
+    LogLevel log_level;
     LogLevel message_level;
     const char* message;
     bool should_log;
-} LoggerTestLazy;
+} TestLazyLogger;
 
-int test_logger_lazy(TestCase* test) {
-    LoggerTestLazy* unit = (LoggerTestLazy*) test->unit;
+int test_group_lazy_logger(TestUnit* unit) {
+    TestLazyLogger* data = (TestLazyLogger*) unit->data;
 
     int state = file_capture(stderr, file_temp);
 
-    Logger* lazy_logger = logger_create(unit->log_level, LOG_TYPE_UNKNOWN, NULL);
+    Logger* lazy_logger = logger_create(data->log_level, LOG_TYPE_UNKNOWN, NULL);
     if (!lazy_logger) {
-        LOG_ERROR("[LoggerTestLazy] Failed to create a logger instance!");
+        LOG_ERROR("[TestLazyLogger] Failed to create a logger instance!");
         file_restore(stderr, file_temp, state);
         return 1;
     }
-    LOG(lazy_logger, unit->message_level, "%s", unit->message);
+    LOG(lazy_logger, data->message_level, "%s", data->message);
     logger_free(lazy_logger);
 
-    const bool match = file_match(file_temp, unit->message);
+    const bool match = file_match(file_temp, data->message);
     file_restore(stderr, file_temp, state);
 
     ASSERT(
-        match == unit->should_log,
-        "[LoggerTestLazy] log_level=%d, message_level=%d, expected='%s', got='%s'",
-        unit->log_level,
-        unit->message_level,
-        unit->should_log ? "present" : "absent",
+        match == data->should_log,
+        "[TestLazyLogger] log_level=%d, message_level=%d, expected='%s', got='%s'",
+        data->log_level,
+        data->message_level,
+        data->should_log ? "present" : "absent",
         match ? "present" : "absent"
     );
 
@@ -308,41 +312,40 @@ int test_logger_lazy(TestCase* test) {
 }
 
 int test_logger_lazy_suite(void) {
-    static LoggerTestLazy cases[] = {
+    static TestLazyLogger data[] = {
         {LOG_LEVEL_DEBUG, LOG_LEVEL_DEBUG, "Lazy logger debug", true},
         {LOG_LEVEL_DEBUG, LOG_LEVEL_ERROR, "Lazy logger error", true},
         {LOG_LEVEL_INFO, LOG_LEVEL_DEBUG, "This should not log", false},
     };
 
-    size_t total_tests = sizeof(cases) / sizeof(LoggerTestLazy);
-
-    TestCase test_cases[total_tests];
-    for (size_t i = 0; i < total_tests; i++) {
-        test_cases[i].unit = &cases[i];
+    size_t count = sizeof(data) / sizeof(TestLazyLogger);
+    TestUnit units[count];
+    for (size_t i = 0; i < count; i++) {
+        units[i].data = &data[i];
     }
 
-    TestContext context = {
-        .total_tests = total_tests,
-        .test_name = "Logger Lazy Initialization",
-        .test_cases = test_cases,
+    TestGroup group = {
+        .name = "Test Lazy Logger Group",
+        .count = count,
+        .units = units,
+        .run = test_group_lazy_logger,
     };
 
-    return test_unit_run(&context, test_logger_lazy, NULL);
+    return test_group_run(&group);
 }
 
 int main(void) {
-    static TestRegister suites[] = {
-        {"Log Level", test_logger_level_suite},
-        {"Log File", test_logger_file_suite},
-        {"Logger Global", test_logger_global_suite},
-        {"Logger Lazy Initialization", test_logger_lazy_suite},
+    TestSuite suites[] = {
+        {"Log Level Suite", test_suite_log_level},
+        {"Log File Suite", test_suite_log_file},
+        {"Global Logger Suite", test_suite_global_logger},
+        {"Lazy Logger Suite", test_logger_lazy_suite},
     };
 
     int result = 0;
-    size_t total = sizeof(suites) / sizeof(TestRegister);
-    for (size_t i = 0; i < total; i++) {
-        result |= test_suite_run(suites[i].name, suites[i].test_suite);
+    size_t count = sizeof(suites) / sizeof(TestSuite);
+    for (size_t i = 0; i < count; i++) {
+        result |= test_suite_run(&suites[i]);
     }
-
     return result;
 }
