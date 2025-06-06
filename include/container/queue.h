c2b3e786a64d5ee4e2c8b3dbc019e5c3003c3fe6
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-05 22:59:36 -0400

    queue(refactor): Add header guards, add generic container ref, and add container prefixes

--- include/container/queue.h
+++ include/container/queue.h
@@ -4,36 +4,39 @@
  * @file include/queue.h
  */
 
-#include "linked_list.h"
+#ifndef DSA_CONTAINER_QUEUE_H
+#define DSA_CONTAINER_QUEUE_H
+
+#include "container/node.h"
 
 /**
  * @brief A sequence of elements held in temporary storage awaiting processing
  */
-typedef struct Queue {
-    LinkedList* list; // Queue implemented as a linked list
-} Queue;
+typedef Container ContainerQueue;
 
 /**
  * @brief Initialize a new queue.
  */
-Queue* queue_create();
+ContainerQueue* container_queue_create(void);
 
 /**
  * @brief Free memory and destroy the queue.
  */
-void queue_free(Queue* queue);
+void container_queue_free(ContainerQueue* queue);
 
 /**
  * @brief Add data to the end of the queue.
  */
-void queue_enqueue(Queue* queue, void* data);
+void container_queue_enqueue(ContainerQueue* queue, void* data);
 
 /**
  * @brief Remove data from the front of the queue.
  */
-void* queue_dequeue(Queue* queue);
+void* container_queue_dequeue(ContainerQueue* queue);
 
 /**
  * @brief View the front element without removing it.
  */
-void* queue_front(const Queue* queue);
+void* container_queue_peek(const ContainerQueue* queue);
+
+#endif // DSA_CONTAINER_QUEUE_H
