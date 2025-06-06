e6e663b11b42b1a5204bbcece065496ca44ea9b0
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-05 22:06:55 -0400

    node(chore): Fix header guards and add doxygen section labels

--- include/container/node.h
+++ include/container/node.h
@@ -4,24 +4,39 @@
  * @file include/container/node.h
  */
 
-#ifndef CONTAINER_NODE_H
-#define CONTAINER_NODE_H
+#ifndef DSA_CONTAINER_NODE_H
+#define DSA_CONTAINER_NODE_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
 /**
+ * @name Node Structures
+ * @{
+ */
+
+/**
  * @brief A point on a stem at which a leaf or leaves are inserted
  *
  * @param object Generic pointer to hold any type of object
  * @param next Pointer to the next node
+ * @param prev Pointer to the previous node
  */
 typedef struct ContainerNode {
     void* object; // Generic pointer to hold any type of object
     struct ContainerNode* next; // Pointer to the next node
     struct ContainerNode* prev; // Pointer to the previous node
 } ContainerNode;
+
+/**
+ * @}
+ */
+
+/**
+ * @name Node Functions
+ * @{
+ */
 
 /**
  * @brief Function to create a new node.
@@ -33,8 +48,12 @@ ContainerNode* container_node_create(void* object);
  */
 void container_node_free(ContainerNode* node);
 
+/**
+ * @}
+ */
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
 
-#endif // CONTAINER_NODE_H
+#endif // DSA_CONTAINER_NODE_H
