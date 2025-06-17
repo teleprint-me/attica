616d78dc049ad58ab0051280a815006ae759c9bc
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-17 19:48:17 -0400

    test_list(feat): Add suite for shared list state and getting indices from values

--- tests/container/test_list.c
+++ tests/container/test_list.c
@@ -2,11 +2,6 @@
  * Copyright © 2023 Austin Berrio
  *
  * @file tests/container/test_list.c
- *
- * @note Keep fixtures and tests as simple as reasonably possible.
- *       - Tests must be kept simple. The simpler, the better.
- *       - Tests must return an integer with 0 on success and 1 on failure.
- *         - I'll admit this is weird, but the shell and cmake test for this.
  */
 
 #include "core/memory.h"
@@ -33,7 +28,7 @@ int test_unit_list_teardown(TestUnit* unit) {
     return 0;
 }
 
-int test_group_list_append_and_get(TestUnit* unit) {
+int test_unit_list_append_and_get_data(TestUnit* unit) {
     TestListData* data = (TestListData*) unit->data;
 
     container_list_append(data->list, &data->value);
@@ -43,7 +38,7 @@ int test_group_list_append_and_get(TestUnit* unit) {
     return 0;
 }
 
-int test_suite_list(void) {
+int test_suite_list_append_and_get_data(void) {
     TestListData data[] = {
         {.value = 5, .expected = 5},
         {.value = 3, .expected = 3},
@@ -57,12 +52,68 @@ int test_suite_list(void) {
     }
 
     TestGroup group = {
-        .name = "Container List",
+        .name = "Append and Get Data",
         .count = count,
         .units = units,
-        .run = test_group_list_append_and_get,
+        .run = test_unit_list_append_and_get_data,
         .before_each = test_unit_list_setup,
         .after_each = test_unit_list_teardown,
+    };
+
+    return test_group_run(&group);
+}
+
+typedef struct TestListIndex {
+    ContainerList* list;
+    size_t expected;
+    int value;
+} TestListIndex;
+
+int test_group_list_setup(TestGroup* group) {
+    ContainerList* list = container_list_create();
+    TestUnit* units = (TestUnit*) group->units;
+    for (size_t i = 0; i < group->count; i++) {
+        TestListIndex* data = (TestListIndex*) (units)[i].data;
+        data->list = list;
+        container_list_append(list, &data->value);
+    }
+    group->shared = (const void*) list;
+    return 0;
+}
+
+int test_group_list_teardown(TestGroup* group) {
+    ContainerList* list = (ContainerList*) group->shared;
+    container_list_free(list);
+    return 0;
+}
+
+int test_unit_list_append_and_get_index(TestUnit* unit) {
+    TestListIndex* data = (TestListIndex*) unit->data;
+    size_t result = container_list_get_index(data->list, &data->value);
+    ASSERT(result == data->expected, "Expected %zu, got %zu", data->expected, result);
+    return 0;
+}
+
+int test_suite_list_append_and_get_index(void) {
+    TestListIndex data[] = {
+        {.value = 5, .expected = 0},
+        {.value = 3, .expected = 1},
+        {.value = 7, .expected = 2},
+    };
+
+    size_t count = sizeof(data) / sizeof(TestListIndex);
+    TestUnit units[count];
+    for (size_t i = 0; i < (count); i++) {
+        (units)[i].data = &(data)[i];
+    }
+
+    TestGroup group = {
+        .name = "Append and Get Index",
+        .count = count,
+        .units = units,
+        .run = test_unit_list_append_and_get_index,
+        .before_all = test_group_list_setup,
+        .after_all = test_group_list_teardown,
     };
 
     return test_group_run(&group);
@@ -75,7 +126,8 @@ int test_suite_list(void) {
  */
 int main(void) {
     TestSuite suites[] = {
-        {"Container List", test_suite_list},
+        {"Append and Get Data", test_suite_list_append_and_get_data},
+        {"Append and Get Index", test_suite_list_append_and_get_index},
     };
 
     int result = 0;
