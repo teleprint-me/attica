b7367b86c61dbee4116eba0827d71769709ceec3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-06-18 19:11:14 -0400

    tests(patch): Add fix for handling unused variables in debug mode
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- tests/sort/test_heap.c
+++ tests/sort/test_heap.c
@@ -13,9 +13,10 @@ static void test_heap_sort_basic() {
     int data[] = {5, 2, 9, 1, 5, 6};
     heap_sort(data, 6);
     int expected[] = {1, 2, 5, 5, 6, 9};
+    (void) expected; // compiler claims this is unused.
     for (size_t i = 0; i < 6; ++i) {
-        assert(data[i] == expected[i]);
-    }
+        assert(data[i] == expected[i]); // it obviously is used here.
+    } // stupid compiler. :p
 }
 
 int main(void) {
